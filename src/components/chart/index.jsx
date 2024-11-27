2c51fbd7e59aff104395e453a562e2b8f91d7880
BuiThanhBinh <binh.s078251@gmail.com>
2024-11-28 00:31:03 +0700

    Apply bar chart success

--- src/components/chart/index.jsx
+++ src/components/chart/index.jsx
@@ -1,16 +1,32 @@
 import React, { useState, useEffect } from "react";
-import { CircularProgress } from "@mui/material";
 import { Chart } from "react-google-charts";
 import axios from "axios";
 import { Typography } from "@mui/material";
 
-export default function Charts(props) {
-  const [codeAnalysitc, setCodeAnalysitc] = useState({});
+const dataDefault = [
+  {
+    promo_code: "None",
+    TOTAL_AMOUNT: 0,
+    PROMO_AMOUNT: 0,
+    COUNT: 0,
+    START_DAY: "2016-08-07T09:22:39.501036Z",
+    END_DAY: "2022-07-31T23:16:43.885323Z",
+  },
+];
 
-  function convertDataToChart(data) {
-    var lst = [["code", "total_amount"]];
+export default function Charts(props) {
+  const [codeAnalysitc, setCodeAnalysitc] = useState(dataDefault);
+
+  function convertDataToChart(data, xAxis, yAxis) {
+    var temp = [yAxis];
+    temp = temp.concat(xAxis);
+    var lst = [temp];
     for (var i = 0; i < data.length; i++) {
-      lst.push([data[i].promo_code, data[i].TOTAL_AMOUNT]);
+      temp = [data[i][yAxis]];
+      xAxis.map((item) => {
+        temp.push(data[i][item]);
+      });
+      lst.push(temp);
     }
     return lst;
   }
@@ -21,8 +37,7 @@ export default function Charts(props) {
         const response = await axios.get(
           "http://localhost:8000/code-analysis/"
         );
-        data = response.data.data;
-
+        var data = response.data;
         setCodeAnalysitc(data);
       } catch (error) {
         console.error(error);
@@ -30,33 +45,6 @@ export default function Charts(props) {
     };
     fetchData();
   }, []);
-
-  const dataChart = [
-    {
-      promo_code: "None",
-      TOTAL_AMOUNT: 37884770000,
-      PROMO_AMOUNT: 0,
-      COUNT: 0,
-      START_DAY: "2016-08-07T09:22:39.501036Z",
-      END_DAY: "2022-07-31T23:16:43.885323Z",
-    },
-    {
-      promo_code: "AZ2022",
-      TOTAL_AMOUNT: 6675745000,
-      PROMO_AMOUNT: 60119641,
-      COUNT: 12005,
-      START_DAY: "2016-08-08T06:25:24.341833Z",
-      END_DAY: "2022-07-29T22:16:37.169906Z",
-    },
-    {
-      promo_code: "BUYMORE",
-      TOTAL_AMOUNT: 4962927000,
-      PROMO_AMOUNT: 44486761,
-      COUNT: 8942,
-      START_DAY: "2016-08-08T03:46:50.958678Z",
-      END_DAY: "2022-07-29T12:48:21.123459Z",
-    },
-  ];
 
   return (
     <div className="grid grid-cols-2 gap-[20px]">
@@ -67,30 +55,52 @@ export default function Charts(props) {
       </div>
       <div className="rounded-lg bg-white mb-4 h-[625px]">
         <Typography variant="h5" className="!font-extrabold ps-8 pt-4 ">
-          Báo cáo doanh thu
+          Báo cáo tình trạng sử dụng mã khuyến mãi
           <Chart
             width={"100%"}
             height={"100%"}
             chartType="BarChart"
             loader={<div>Loading Chart</div>}
-            data={convertDataToChart(dataChart)}
+            data={convertDataToChart(
+              codeAnalysitc,
+              ["TOTAL_AMOUNT"],
+              "promo_code"
+            )}
             options={{
               legend: { position: "bottom" },
+              title: "Biểu đồ tổng giá trị đơn hàng có mã khuyến mãi",
+              vAxis: { title: "Mã khuyến mãi" },
+              bar: { groupWidth: "50%" },
             }}
-            rootProps={{ "data-testid": "6" }}
-            chartPackages={["corechart", "controls"]}
-            render={({ renderChart }) => {
-              return (
-                <div
-                  style={{
-                    display: "flex",
-                    height: "100%",
-                    flexDirection: "column",
-                  }}
-                >
-                  <div>{renderChart()}</div>
-                </div>
-              );
+          />
+          <Chart
+            width={"100%"}
+            height={"100%"}
+            chartType="BarChart"
+            loader={<div>Loading Chart</div>}
+            data={convertDataToChart(
+              codeAnalysitc,
+              ["PROMO_AMOUNT"],
+              "promo_code"
+            )}
+            options={{
+              legend: { position: "bottom" },
+              title: "Biểu đồ tổng giá trị đơn hàng có mã khuyến mãi",
+              vAxis: { title: "Mã khuyến mãi" },
+              bar: { groupWidth: "50%" },
+            }}
+          />
+          <Chart
+            width={"100%"}
+            height={"100%"}
+            chartType="BarChart"
+            loader={<div>Loading Chart</div>}
+            data={convertDataToChart(codeAnalysitc, ["COUNT"], "promo_code")}
+            options={{
+              legend: { position: "bottom" },
+              title: "Biểu đồ tổng giá trị đơn hàng có mã khuyến mãi",
+              vAxis: { title: "Mã khuyến mãi" },
+              bar: { groupWidth: "50%" },
             }}
           />
         </Typography>
